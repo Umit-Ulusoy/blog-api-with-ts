7f9682b06c79de6fc89dd3dc6b471c227732481c
Ümit ULUSOY <88715653+Umit-Ulusoy@users.noreply.github.com>
2025-05-06 15:42:54 +0300

    feat: create user model for user actions

---
+++ src/models/User.ts
@@ -0,0 +1,56 @@
+import { Schema, model, Document } from "mongoose";
+import bcrypt from "bcryptjs";
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+
+  matchPassword(enteredPassword: string): Promise<boolean>;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.matchPassword = async function (
+  enteredPassword: string
+): Promise<boolean> {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = model<IUser>("User", userSchema);
+
+export default User;
