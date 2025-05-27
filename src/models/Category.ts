5d1dc89fd1f4dcb3f85ca5b91879395ad8ba4ff0
Ümit ULUSOY <88715653+Umit-Ulusoy@users.noreply.github.com>
2025-05-27 18:05:26 +0300

    feat: create category model

---
+++ src/models/Category.ts
@@ -0,0 +1,37 @@
+import { Schema, model, Document } from "mongoose";
+import slugify from "slugify";
+
+export interface ICategory extends Document {
+  name: string;
+  slug: string;
+}
+
+const categorySchema = new Schema<ICategory>(
+  {
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lower: true
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    },
+  },
+  { timestamps: true }
+);
+
+categorySchema.pre("save", function (next) {
+  if (this.isModified("name")) {
+    this.slug = slugify(this.name, {
+        lower: true,
+        strict: true
+    });
+  }
+  next();
+});
+
+export default model<ICategory>("Category", categorySchema);
